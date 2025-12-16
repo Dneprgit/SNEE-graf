d767e66e868bc8d14e171ed9b1ebb6bc5372a836
Egor Dnepr <end2040@gmail.com>
2025-12-16 16:33:57 +0300

    dev56_local_ok

--- web/frontend/src/services/api.js
+++ web/frontend/src/services/api.js
@@ -3,14 +3,20 @@ import axios from 'axios';
 // Используем пустой baseURL - все пути указаны полностью в методах
 // Это позволяет работать через nginx reverse proxy без необходимости задавать VITE_API_URL
 
-//const API_BASE_URL = import.meta.env.VITE_API_URL || '';
+
 // !!!!!!!! запускаем на сервере
+//const API_BASE_URL = import.meta.env.VITE_API_URL || '';
+
+//const API_BASE_URL = '';
+
 //const API_BASE_URL = import.meta.env.VITE_API_URL || 
 //  (import.meta.env.MODE === 'production' ? '' : 'http://localhost:8002');
+
+//const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:8002';
+
 // !!!!!!!! локально запускаем
 const API_BASE_URL = 'http://localhost:8002';
 
-//const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:8001';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
